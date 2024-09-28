f449f556b88b7701f0511f522168c84815d5fc3b
itimoreo <pol.lopezpro@gmail.com>
2024-09-28 16:53:09 +0200

    Setup Basic application and Entry Point

---
+++ GameEngine/src/GameEngine.h
@@ -0,0 +1,10 @@
+#pragma once
+
+//For use by GameEngine applications
+
+#include <stdio.h>
+#include "GameEngine/Application.h"
+
+// ---Entry Point---------------------
+#include  "GameEngine/EntryPoint.h"
+//---------------------------
---
+++ GameEngine/src/GameEngine/Application.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Core.h"
+
+
+namespace GameEngine
+{
+	class GAMEENGINE_API Application
+	{
+	public:
+		Application();
+		virtual ~Application();
+
+		void Run();
+	};
+
+
+	//To be defined in CLIENT
+	Application* CreateApplication();
+
+
+}
+
+
+
